30c3e97b236699285b11fef114df0da9b36f8d95
Krishan Patel <krishan711@gmail.com>
2021-02-10 21:26:48 +0000

    .

--- src/divergent/api/api_v1.py
+++ src/divergent/api/api_v1.py
@@ -1,10 +1,5 @@
-from fastapi import APIRouter
-from fastapi import Request
-
 from core.api.kiba_router import KibaRouter
-
 
 def create_api_v1() -> KibaRouter():
     router = KibaRouter()
-
     return router
